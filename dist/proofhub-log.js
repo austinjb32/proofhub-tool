5f898392d81d0f4d49e35c6c77609f0e8de2651b
austinj32 <austin.j@hubspire.com>
2023-11-24 12:50:27 +0530

    Sixth Commit

--- dist/proofhub-log.js
+++ dist/proofhub-log.js
@@ -2,5 +2,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
-const result = (0, child_process_1.spawnSync)('./bin/proofhub-log.sh', process.argv.slice(2), { stdio: 'inherit' });
-process.exit(result.status ? result.status : undefined);
+const fs = require("fs");
+const path = require("path");
+// Path to the shell script
+const scriptPath = path.join(__dirname, "..", 'bin', 'proofhub-log.sh');
+// Check if the script file exists
+if (fs.existsSync(scriptPath)) {
+    // Add execute permissions to the script
+    const chmodResult = (0, child_process_1.spawnSync)('chmod', ['+x', scriptPath]);
+    if (chmodResult.status === 0) {
+        console.log(`Execute permissions added to ${scriptPath}`);
+    }
+    else {
+        console.error(`Failed to add execute permissions to ${scriptPath}`);
+    }
+    // Execute the script
+    const result = (0, child_process_1.spawnSync)(scriptPath, process.argv.slice(2), { stdio: 'inherit', shell: true });
+    process.exit(result.status ? result.status : undefined);
+}
+else {
+    console.error(`Script ${scriptPath} not found.`);
+    process.exit(1);
+}
--- src/proofhub-log.ts
+++ src/proofhub-log.ts
@@ -1,7 +1,27 @@
 #!/usr/bin/env node
 
 import { spawnSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const result = spawnSync('./bin/proofhub-log.sh', process.argv.slice(2), { stdio: 'inherit' });
+// Path to the shell script
+const scriptPath = path.join(__dirname,"..", 'bin', 'proofhub-log.sh');
 
-process.exit(result.status?result.status:undefined);
+// Check if the script file exists
+if (fs.existsSync(scriptPath)) {
+  // Add execute permissions to the script
+  const chmodResult = spawnSync('chmod', ['+x', scriptPath]);
+
+  if (chmodResult.status === 0) {
+    console.log(`Execute permissions added to ${scriptPath}`);
+  } else {
+    console.error(`Failed to add execute permissions to ${scriptPath}`);
+  }
+
+  // Execute the script
+  const result = spawnSync(scriptPath, process.argv.slice(2), { stdio: 'inherit', shell:true });
+  process.exit(result.status ? result.status : undefined);
+} else {
+  console.error(`Script ${scriptPath} not found.`);
+  process.exit(1);
+}
